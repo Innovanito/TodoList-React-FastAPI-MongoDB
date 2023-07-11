34f2a293f76218519e2a77ba02acd44156d5bddb
Innovanito <khg05311@naver.com>
2023-07-11 19:07:54 +0900

    bootstrap applied to UI

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css' 
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className=' justify-content-center'>
+        <h1 className='card text-white bg-primary'>
+          Task Manager
+        </h1>
+      </div>
+      hey
+      farm stack
+    </div>
+  );
+}
+
+export default App;
